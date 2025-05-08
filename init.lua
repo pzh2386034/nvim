572cc11fac8512ec484493aada202a234732c266
zeuspan <zeuspan@tencent.com>
2025-05-08 08:33:01 +0000

    add fea lsp,complete,comment,autopairs

--- init.lua
+++ init.lua
@@ -2,3 +2,40 @@ require("core.option")
 require("core.keymaps")
 require("plugins.plugins-setup")
 require("plugins.nvim-treesitter")
+
+require("plugins/mason-lspconfig")
+require("plugins.cmp")
+require('Comment').setup({
+    toggler = {
+        ---Line-comment toggle keymap
+        line = 'gcc',
+        ---Block-comment toggle keymap
+        block = 'gbc',
+    },
+    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+        ---Line-comment keymap
+        line = 'gc',
+        ---Block-comment keymap
+        block = 'gb',
+    },
+    ---LHS of extra mappings
+    extra = {
+        ---Add comment on the line above
+        above = 'gcO',
+        ---Add comment on the line below
+        below = 'gco',
+        ---Add comment at the end of line
+        eol = 'gcA',
+    },
+})
+
+require("plugins.nvim-autopairs")
+require("plugins.bufferline")
+
+local builtin = require('telescope.builtin')
+-- 进入telescope页面会是插入模式，回到正常模式就可以用j和k来移动了
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})  -- 环境里要安装ripgrep
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -27,8 +27,34 @@ return require('packer').startup(function(use)
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'p00f/nvim-ts-rainbow'
+  use 'nvim-treesitter/nvim-treesitter' -- 语法高亮
+  use 'p00f/nvim-ts-rainbow' -- 配合treesitter，不同括号颜色区分
+
+
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",  -- 这个相当于mason.nvim和lspconfig的桥梁
+    "neovim/nvim-lspconfig"
+  }
+    -- 自动补全
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "L3MON4D3/LuaSnip" -- snippets引擎，不装这个自动补全会出问题
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
+  use "hrsh7th/cmp-path" -- 文件路径
+
+  use "numToStr/Comment.nvim" -- gcc和gc注释
+  use "windwp/nvim-autopairs" -- 自动补全括号
+
+  use "akinsho/bufferline.nvim" -- buffer分割线
+  --   use "lewis6991/gitsigns.nvim" -- 左则git提示
+
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
   if packer_bootstrap then
     require('packer').sync()
