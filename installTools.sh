75a800d0cb3356494be2c01be2bb09f6403c9bd9
zeuspan <zeuspan@tencent.com>
2025-05-12 01:51:45 +0000

    add install evn script

---
+++ installTools.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -L git.io/antigen > antigen.zsh
+sudo apt install ripgrep zsh neovim fzf fd-find fzy 
+sudo apt install universal-ctags
+sudo apt install python-pip3 python3.12-venv
