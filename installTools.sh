20580d8b4f8496133d31eec347d68e33e4b271f9
zeuspan <zeuspan@tencent.com>
2025-05-15 07:29:01 +0000

    Other fea:add antigen.zsh, dotzshrc

--- installTools.sh
+++ installTools.sh
@@ -3,3 +3,14 @@ curl -L git.io/antigen > antigen.zsh
 sudo apt install ripgrep zsh neovim fzf fd-find fzy 
 sudo apt install universal-ctags
 sudo apt install python-pip3 python3.12-venv
+sudo apt install zoxide
+
+
+curl -L git.io/antigen > ~/wks/antigen/antigen.zsh
+ln -sf ~/.config/dotzshrc ~/.zshrc
+chsh -s /usr/bin/zsh
+
+### Macos
+
+#curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
